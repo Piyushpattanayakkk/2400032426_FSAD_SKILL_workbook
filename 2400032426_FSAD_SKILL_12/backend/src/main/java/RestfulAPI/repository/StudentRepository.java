556ce0640b9355faf38ad401787d81b7e9fe7dfb
Piyushpattanayakkk <pattnayakpiyush0@gmail.com>
2026-04-09 23:46:41 +0530

    Delete 2400032426_FSAD_SKILL_12 directory

--- 2400032426_FSAD_SKILL_12/backend/src/main/java/RestfulAPI/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package RestfulAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import RestfulAPI.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
